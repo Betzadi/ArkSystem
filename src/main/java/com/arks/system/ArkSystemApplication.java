3f1e0c6f5c7c9cc0072e56d53b6ea1e7362841d9
Betzadi <desarrollo.dos.cbi@gmail.com>
2023-08-24 22:54:17 -0600

    Feature/update:initialized project

---
+++ src/main/java/com/arks/system/ArkSystemApplication.java
@@ -0,0 +1,13 @@
+package com.arks.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkSystemApplication.class, args);
+	}
+
+}
